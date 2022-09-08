32b7d3af1234145f4584d013318c549888dd5943
Oliver Zeigermann <oliver.zeigermann@openknowledge.de>
2022-09-08 10:46:25 +0200

    bash as default shell and lab as alternative

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 source /root/.bashrc
 
-conda activate mlops-workshop-d2d
-
-jupyter notebook --allow-root --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
+export SHELL=/bin/bash
+jupyter notebook --allow-root --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''&
+jupyter lab --port 8889 --allow-root --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
