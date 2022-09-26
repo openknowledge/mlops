b5528b976a735fe40908745afcc1b1d7ea1180d1
Oliver Zeigermann <oliver.zeigermann@openknowledge.de>
2022-09-26 18:35:47 +0200

    no additional start of lab server necessary (runs on same port)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,3 @@ source /root/.bashrc
 
 export SHELL=/bin/bash
 jupyter notebook --allow-root --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''&
-jupyter lab --port 8889 --allow-root --ip 0.0.0.0 --NotebookApp.token='' --NotebookApp.password=''
